efe4fd4eed34880e1b6e467642dc21f9eb331692
glemi <cnyffeler@gmail.com>
2013-10-06 21:04:46 +0200

    some earlier changes made at epfl on windows

--- datasetmodel.cpp
+++ datasetmodel.cpp
@@ -7,24 +7,44 @@ DataSetModel::DataSetModel(QObject *parent) :
 
 }
 
-Qt::ItemFlags flags(const QModelIndex &index) const
+Qt::ItemFlags DataSetModel::flags(const QModelIndex &index) const
 {
     return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
 }
 
-QVariant data(const QModelIndex &index, int role) const
+QVariant DataSetModel::data(const QModelIndex &index, int role) const
+{
+    switch (role) 
+    {
+    case Qt::DisplayRole:
+        
+        
+        
+        break;
+    default:
+        break;
+    }
+    
+    
+    return QVariant();
+}
+
+QVariant DataSetModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
     return QVariant();
 }
 
-QVariant headerData(int section, Qt::Orientation orientation, int role) const
-{
-    return QVariant();
-}
-
-int rowCount(const QModelIndex &parent) const
+int DataSetModel::columnCount(const QModelIndex &parent) const
 {
     return 0;
+}
+
+QModelIndex DataSetModel::index(int row, int column, const QModelIndex &parent) const
+{
+}
+
+QModelIndex DataSetModel::parent(const QModelIndex &child) const
+{
 }
 
 int DataSetModel::rowCount(const QModelIndex &parent) const
--- datasetmodel.h
+++ datasetmodel.h
@@ -13,6 +13,11 @@ public:
     QVariant data(const QModelIndex &index, int role) const;
     QVariant headerData(int section, Qt::Orientation orientation, int role) const;
     int rowCount(const QModelIndex &parent) const;
+    int columnCount(const QModelIndex &parent) const;
+    
+    QModelIndex index(int row, int column, const QModelIndex &parent) const;
+    QModelIndex parent(const QModelIndex &child) const;
+    
 
 signals:
     
